326a50a91f7baca9828b86f70c30b770fe82cf88
huangxiaoyong_6057 <hxy_computer@stu.xjtu.edu.cn>
2025-06-14 21:59:52 +0800

    GRPO

---
+++ GRPO/grpo_train.py
@@ -0,0 +1,17 @@
+from transformers import AutoModelForCausalLM, AutoModel, AutoModelForSequenceClassification, AutoTokenizer, PreTrainedModel
+from dataclasses import dataclass
+from typing import Optional, Union, Tuple
+import random
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from typing import Callable, Dict, List, Optional, Tuple, Union, Any
+from copy import deepcopy
+from datasets import load_dataset
+from reward_func import *
+import os
+
+
+if 
