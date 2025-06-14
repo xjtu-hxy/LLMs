27bd1543b7a053546d62072d9bb3bca5f4f59551
huangxiaoyong_6057 <hxy_computer@stu.xjtu.edu.cn>
2025-06-14 21:54:07 +0800

    6月14

--- PPO/ppo_train.py
+++ PPO/ppo_train.py
@@ -84,11 +84,252 @@ class ExperienceBuffer:
     def __getitem__(self, index):
         return self.buffer[index]
 
+def compute_approx_kl(
+    log_probs: torch.Tensor,
+    ref_log_probs: torch.Tensor,
+    action_mask: Optional[torch.Tensor] = None,
+):
+
+    log_ratio = log_probs.float() - ref_log_probs.float()
+    if action_mask is not None:
+        log_ratio = log_ratio * action_mask
+
+    return log_ratio
+
+def compute_rewards(kl, r, action_mask, kl_ctl, clip_reward_value):
+
+    kl_divergence_estimate = -kl_ctl * kl
+    rewards = kl_divergence_estimate
+
+    ends = action_mask.sum(1) + 1
+    
+    if not isinstance(clip_reward_value, torch.Tensor):
+        clip_reward_value = torch.tensor(clip_reward_value).to(r.device)
+
+    reward_clip = torch.clamp(r, -clip_reward_value,
+                                clip_reward_value)
+    batch_size = r.size(0)
+    for j in range(batch_size):
+        rewards[j, :ends[j]][-1] += reward_clip[j, 0]
+
+    return rewards
+
+def get_advantages_and_returns(
+        values: torch.Tensor,
+        rewards: torch.Tensor,
+        action_mask: torch.Tensor,
+        gamma: float,
+        lambd: float):
+    
+    lastgaelam = 0
+    advantages_reversed = []
+    response_length = rewards.size(1)
+    
+    if action_mask is not None:
+        values = action_mask * values
+        rewards = action_mask * rewards
+
+    for t in reversed(range(response_length)):
+        nextvalues = values[:, t + 1] if t < response_length - 1 else 0.0
+        delta = rewards[:, t] + gamma * nextvalues - values[:, t]
+        lastgaelam = delta + gamma * lambd * lastgaelam
+        advantages_reversed.append(lastgaelam)
+    advantages = torch.stack(advantages_reversed[::-1], dim=1)
+    returns = advantages + values
+    return advantages.detach(), returns
+    
+def generate_experiences(samples_list):
+
+    actor_model.eval()
+    ref_model.eval()
+    reward_model.eval()
+    critic_model.eval()
+
+    experiences = []
+    
+    for samples in samples_list:
+        seqs = samples.seqs
+        attention_mask = samples.attention_mask
+        action_mask = samples.action_mask
+        num_actions = samples.num_actions
+        with torch.no_grad():
+            # 计算策略模型输出token的概率
+            output = actor_model(seqs, attention_mask=attention_mask)
+            logits = output.logits
+            log_probs = F.log_softmax(logits[:, :-1, :], dim=-1)
+            log_probs_labels = log_probs.gather(dim=-1, index=seqs[:, 1:].unsqueeze(-1))
+            action_log_probs = log_probs_labels.squeeze(-1)[:, -num_actions:]
+            #计算参考模型输出token的概率
+            ref_output = ref_model(seqs, attention_mask=attention_mask)
+            ref_logits = ref_output.logits
+            ref_log_probs = F.log_softmax(ref_logits[:, :-1, :], dim=-1)
+            ref_log_probs_labels = ref_log_probs.gather(dim=-1, index=seqs[:, 1:].unsqueeze(-1))
+            ref_action_log_probs = ref_log_probs_labels.squeeze(-1)[:, -num_actions:]
+            # 计算价值
+            value = critic_model.forward(seqs, attention_mask, num_actions).to(device)
+            # 转换成文本
+            seq_texts = actor_tokenizer.batch_decode(seqs, skip_special_tokens=True)
+            # 计算奖励模型的奖励值
+            reward_model_inputs = reward_tokenizer(seq_texts, return_tensors="pt", padding=True)
+            r = reward_model(**reward_model_inputs.to(device)).logits # 奖励模型的输出，相当于生成最后一个token的奖励（结果奖励模型）
+            # 计算kl散度
+            kl = compute_approx_kl(
+                    action_log_probs,
+                    ref_action_log_probs,
+                    action_mask=action_mask).to(device)
+            # 计算实际奖励
+            rewards = compute_rewards(kl, r, action_mask, kl_ctl=0.1, clip_reward_value=0.2)
+            # 计算优势和回报
+            advantages, returns = get_advantages_and_returns(value, rewards, action_mask, gamma=0.1, lambd=0.2)
+        # actor_model.train()
+        # critic_model.train()
+
+        experiences.append(Experience(seqs,
+                    action_log_probs.detach(),
+                    value.detach(),
+                    returns.detach(),
+                    advantages.detach(),
+                    attention_mask,
+                    action_mask,
+                    r.detach(),
+                    samples.response_length,
+                    samples.total_length,
+                    num_actions,
+                    kl.detach(),
+        ))
+
+    return experiences
+
+def generate_samples(prompts, model, max_length, max_new_tokens, n_samples_per_prompt, micro_rollout_batch_size):
+    samples_list = []
+    model.eval()
+    all_prompts = sum([[prompt]*n_samples_per_prompt for prompt in prompts], [])
+    for i in range(0, len(all_prompts), micro_rollout_batch_size):
+        prompts = all_prompts[i:i+micro_rollout_batch_size]
+        inputs = actor_tokenizer(prompts, padding='max_length', max_length=max_length, truncation=True, return_tensors='pt')
+        input_ids = inputs['input_ids']
+        seqs = model.generate(**inputs.to(device), 
+                            max_new_tokens = max_new_tokens, 
+                            eos_token_id = eos_token_id, 
+                            pad_token_id = pad_token_id)
+        if seqs.size(1) >= max_new_tokens + max_length:
+            seqs = seqs[:, :max_new_tokens + max_length]
+        else:
+            seqs = torch.cat([seqs, torch.full((seqs.size(0), max_new_tokens + max_length - seqs.size(1)), fill_value=pad_token_id, device=seqs.device)], dim=1)
+            
+        attention_mask = (seqs.ne(pad_token_id)).to(dtype=torch.long)
+        ans = seqs[:, input_ids.size(1):]
+        action_mask = (ans.ne(eos_token_id) & ans.ne(pad_token_id)).to(dtype=torch.long)
+       
+
+        samples = Samples(
+            seqs=seqs,
+            attention_mask=attention_mask,
+            action_mask=action_mask,
+            num_actions=action_mask.size(1),
+            packed_seq_lens=None,
+            response_length=action_mask.float().sum(dim=-1),
+            total_length=attention_mask.float().sum(dim=-1),
+        )
+        samples_list.append(samples)
+
+    return samples_list
+
 @dataclass
 class Samples:
     seqs: torch.Tensor
     attention_mask: Optional[torch.LongTensor]
+    action_mask: Optional[torch.BoolTensor]
+    num_actions: Union[int, torch.Tensor]
+    packed_seq_lens: Optional[torch.Tensor]
+    response_length: torch.Tensor
+    total_length: torch.Tensor
 
+@dataclass
+class Experience:
+    seqs: torch.Tensor
+    action_log_probs: torch.Tensor
+    values: torch.Tensor
+    returns: Optional[torch.Tensor]
+    advantages: Optional[torch.Tensor]
+    attention_mask: Optional[torch.LongTensor]
+    action_mask: Optional[torch.BoolTensor]
+    reward: torch.Tensor
+    response_length: torch.Tensor
+    total_length: torch.Tensor
+    num_actions: Union[int, torch.Tensor]
+    kl: Optional[torch.Tensor] = None
+
+def compute_approx_kl(
+    log_probs: torch.Tensor,
+    ref_log_probs: torch.Tensor,
+    action_mask: Optional[torch.Tensor] = None,
+):
+
+    log_ratio = log_probs.float() - ref_log_probs.float()
+    if action_mask is not None:
+        log_ratio = log_ratio * action_mask
+
+    return log_ratio
+
+def compute_policy_loss(log_probs, old_log_probs, advantages, action_mask=None, clip_eps=0.2):
+    ratio = (log_probs - old_log_probs).exp()
+    surr1 = ratio * advantages
+    surr2 = ratio.clamp(1.0 - clip_eps, 1.0 + clip_eps) * advantages
+    loss = -torch.min(surr1, surr2)
+    if action_mask is None:
+        return loss.mean(-1).mean()
+    return ((loss * action_mask).sum(-1) / action_mask.sum(-1)).mean()
+
+def compute_value_loss(values, old_values, returns, action_mask=None, clip_eps: float = None):
+    if clip_eps is not None:
+        values_clipped = old_values + (values - old_values).clamp(-clip_eps, clip_eps)
+        surr1 = (values_clipped - returns) ** 2
+        surr2 = (values - returns) ** 2
+        loss = torch.max(surr1, surr2)
+    else:
+        loss = (values - returns) ** 2
+        
+    if action_mask is None:
+        return loss.mean(-1).mean()
+    return ((loss * action_mask).sum(-1) / action_mask.sum(-1)).mean()
+
+def train_step(experience, steps):
+    
+    actor_model.train()
+    optimizer_actor.zero_grad()
+
+    sequences = experience.seqs
+    old_action_log_probs = experience.action_log_probs
+    advantages = experience.advantages
+    num_actions = experience.num_actions
+    attention_mask = experience.attention_mask
+    action_mask = experience.action_mask
+    old_values = experience.values
+    returns = experience.returns
+    
+    logits = actor_model(
+            sequences,
+            attention_mask=attention_mask).logits
+    
+    log_probs = F.log_softmax(logits[:, :-1, :], dim=-1)
+    log_probs_labels = log_probs.gather(dim=-1, index=sequences[:, 1:].unsqueeze(-1))
+    action_log_probs = log_probs_labels.squeeze(-1)[:, -num_actions:]
+  
+    policy_loss = compute_policy_loss(action_log_probs, old_action_log_probs, advantages,action_mask=action_mask)
+    policy_loss.backward()
+    optimizer_actor.step()  
+    writer.add_scalar("policy_loss", policy_loss.item(), steps)
+    
+    critic_model.train()
+    optimizer_critic.zero_grad()
+    values = critic_model.forward(sequences, attention_mask, num_actions)
+    value_loss = compute_value_loss(values, old_values, returns, action_mask)
+    value_loss.backward()
+    optimizer_critic.step()
+    writer.add_scalar("value_loss", value_loss.item(), steps)
+    print(f"step: {steps}  policy_loss: {policy_loss.item():.4f}  value_loss: {value_loss.item():.4f}")
+    
 
 def train():
     # 初始化经验池
@@ -97,8 +338,20 @@ def train():
     for episode in range(episodes):
         for rand_prompts in prompts_dataloader:
             # 生成样本（获取模型推理结果）
-            samples = 
+            samples = generate_samples(rand_prompts, actor_model, max_length, max_new_tokens, n_samples_per_prompt, micro_rollout_batch_size)
             # 生成经验（获取优势、奖励、回报等）
+            experiences = generate_experiences(samples)
+            buffer.append(experiences)
+            dataloader = DataLoader(buffer, batch_size=micro_train_batch_size, shuffle=True, collate_fn=collate_fn)
+            torch.cuda.empty_cache()
+            for epoch in range(max_epochs):
+                for experience in dataloader:
+                    train_step(experience, steps)
+                    steps += 1
+            
+            buffer.clear()
+        
+            torch.cuda.empty_cache()
 
 if __name__ == '__main__':
     device = ' cuda' if torch.cuda.is_available() else 'cpu'
